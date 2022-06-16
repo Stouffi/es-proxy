1c9cb35930c0f01b5871f385cc0b4b99edb8bd34
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-16 22:50:53 +0200

    fixes path for middleware

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,7 @@ const esUrl = process.env['ES_URL']!
 const accessKeyId = process.env['AWS_ACCESS_KEY_ID']
 const secretAccessKey = process.env['AWS_SECRET_ACCESS_KEY']
 
-app.use('/*', proxy(esUrl, {proxyReqOptDecorator(_, srcReq) {
+app.use(proxy(esUrl, {proxyReqOptDecorator(_, srcReq) {
     aws4.sign(srcReq, {accessKeyId, secretAccessKey})
     return srcReq
 },}));
