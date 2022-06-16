86d427f351ef2b88c4fd94b7a67291d25aa10f4b
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-16 18:01:30 +0200

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import proxy from 'express-http-proxy'
+import exp from  'express'
+import aws4 from 'aws4'
+
+const app = exp()
+
+const esUrl = process.env['ES_URL']!
+const accessKeyId = process.env['AWS_ACCESS_KEY_ID']
+const secretAccessKey = process.env['AWS_SECRET_ACCESS_KEY']
+
+app.use('/*', proxy(esUrl, {proxyReqOptDecorator(_, srcReq) {
+    aws4.sign(srcReq, {accessKeyId, secretAccessKey})
+    return srcReq
+},}));
