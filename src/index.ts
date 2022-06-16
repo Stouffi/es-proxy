18648253e32ff9197dc8a4e25c14bbd2e1849ba2
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-16 23:41:17 +0200

    only sign with host

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,12 @@ const esUrl = process.env['ES_URL']!
 const accessKeyId = process.env['AWS_ACCESS_KEY_ID']
 const secretAccessKey = process.env['AWS_SECRET_ACCESS_KEY']
 
-app.use(proxy(esUrl, {proxyReqOptDecorator(reqOpts, srcReq) {
-    console.log('REQ OPTS', reqOpts)
-    aws4.sign(reqOpts as aws4.Request, {accessKeyId, secretAccessKey})
-    console.log('AFTER', reqOpts)
-    return reqOpts
+app.use(proxy(esUrl, {proxyReqOptDecorator(reqOpts) {
+    const reqOptions:aws4.Request = {host: reqOpts.host ?? undefined}
+    console.log('REQ OPTS', reqOptions)
+    aws4.sign(reqOptions, {accessKeyId, secretAccessKey})
+    console.log('AFTER', reqOptions)
+    return reqOptions
 },}));
 
 app.listen(process.env['PORT'], () => {
