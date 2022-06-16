9b0a17c5db62caccee78839a6152e94a749bed4d
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-17 00:06:34 +0200

    only use properties useful

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,33 @@
-import proxy from 'express-http-proxy'
-import exp from  'express'
-import aws4 from 'aws4'
+import proxy from "express-http-proxy";
+import exp from "express";
+import aws4 from "aws4";
 
-const app = exp()
+const app = exp();
 
-const esUrl = process.env['ES_URL']!
-const accessKeyId = process.env['AWS_ACCESS_KEY_ID']
-const secretAccessKey = process.env['AWS_SECRET_ACCESS_KEY']
+const esUrl = process.env["ES_URL"]!;
+const accessKeyId = process.env["AWS_ACCESS_KEY_ID"];
+const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"];
 
-app.use(proxy(esUrl, {proxyReqOptDecorator(reqOpts) {
-    const reqOptions:aws4.Request = {host: reqOpts.host ?? undefined}
-    console.log('REQ OPTS', reqOptions)
-    aws4.sign(reqOptions, {accessKeyId, secretAccessKey})
-    console.log('AFTER', reqOptions)
-    return reqOptions
-},}));
+app.use(
+  proxy(esUrl, {
+    proxyReqOptDecorator(reqOpts, request) {
+      const headers = reqOpts.headers ?? {};
+      const { ["Content-Type"]: contentType, Date: dateHeader } = headers;
+      const reqOptions: aws4.Request = {
+        host: reqOpts.host ?? undefined,
+        method: reqOpts.method,
+        path: reqOpts.path ?? undefined,
+        body: request.body,
+        headers: { "Content-Type": contentType, Date: dateHeader },
+      };
+      console.log("REQ OPTS", reqOpts.headers, reqOptions);
+      aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
+      console.log("AFTER", reqOptions);
+      return reqOptions;
+    },
+  })
+);
 
-app.listen(process.env['PORT'], () => {
-    console.log('proxy started')
-})
+app.listen(process.env["PORT"], () => {
+  console.log("proxy started");
+});
