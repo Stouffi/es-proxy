afbfa8d9569f12b0b072f628b8092ae60de259b2
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-17 10:02:28 +0200

    fixes undefined instead of no key

--- src/index.ts
+++ src/index.ts
@@ -4,8 +4,8 @@ import aws4 from "aws4";
 
 const app = exp();
 const esUrl = process.env["ES_URL"]!;
-const accessKeyId = process.env["AWS_ACCESS_KEY_ID"];
-const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"];
+const accessKeyId = process.env["AWS_ACCESS_KEY_ID"]!;
+const secretAccessKey = process.env["AWS_SECRET_ACCESS_KEY"]!;
 
 app.use(exp.raw({ type: "application/json" }));
 
@@ -15,15 +15,23 @@ app.use(
       const headers = reqOpts.headers ?? {};
       const { ["content-type"]: contentType, date: dateHeader } = headers;
       const reqOptions: aws4.Request = {
-        host: reqOpts.host ?? undefined,
-        method: reqOpts.method,
-        path: reqOpts.path ?? undefined,
-        body: request.body instanceof Buffer ? request.body : undefined,
         headers: Object.assign(
           contentType ? { "Content-Type": contentType } : {},
           dateHeader ? { Date: dateHeader } : {}
         ),
       };
+      if (request.body instanceof Buffer) {
+        reqOptions.body = request.body;
+      }
+      if (reqOpts.host) {
+        reqOptions.host = reqOpts.host
+      }
+      if (reqOpts.path) {
+        reqOptions.path = reqOpts.path
+      }
+      if (reqOpts.method) {
+        reqOptions.method = reqOpts.method
+      }
       console.log("REQ OPTS", reqOpts.headers, reqOptions);
       aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
       console.log("AFTER", reqOptions);
