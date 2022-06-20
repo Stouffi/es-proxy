cdebdd8dda09880b6a14e0322336263086b2d850
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-20 12:01:05 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -11,46 +11,46 @@ app.use(exp.raw({ type: "application/json" }));
 
 app.use(
   proxy(esUrl, {
-    // proxyReqOptDecorator(reqOpts, request) {
-    //   const headers = reqOpts.headers ?? {};
-    //   const { ["content-type"]: contentType, date: dateHeader } = headers;
-    //   const reqOptions: aws4.Request = {
-    //     headers: Object.assign(
-    //       contentType ? { "Content-Type": contentType } : {},
-    //       dateHeader ? { Date: dateHeader } : {}
-    //     ),
-    //   };
-    //   if (request.body instanceof Buffer) {
-    //     reqOptions.body = request.body;
-    //   }
-    //   if (reqOpts.host) {
-    //     reqOptions.host = reqOpts.host
-    //   }
-    //   if (reqOpts.path) {
-    //     reqOptions.path = reqOpts.path
-    //   }
-    //   if (reqOpts.method) {
-    //     reqOptions.method = reqOpts.method
-    //   }
-    //   console.log("REQ OPTS", reqOpts.headers, reqOptions);
-    //   aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
-    //   console.log("AFTER", reqOptions);
-    //   return reqOptions;
-    // },
     proxyReqOptDecorator(reqOpts, request) {
-      console.log("BEFORE", reqOpts);
-      const res = aws4.sign(
-        {
-          host: reqOpts.host!,
-          path: reqOpts.path!,
-          headers: {},
-          body: request.body instanceof Buffer ? request.body : '',
-        },
-        { accessKeyId, secretAccessKey }
-      );
-      console.log("AFTER", res);
-      return res;
+      const headers = reqOpts.headers ?? {};
+      const { ["content-type"]: contentType, date: dateHeader } = headers;
+      const reqOptions: aws4.Request = {
+        headers: Object.assign(
+          contentType ? { "Content-Type": contentType } : {},
+          dateHeader ? { Date: dateHeader } : {}
+        ),
+      };
+      if (request.body instanceof Buffer) {
+        reqOptions.body = request.body;
+      }
+      if (reqOpts.host) {
+        reqOptions.host = reqOpts.host
+      }
+      if (reqOpts.path) {
+        reqOptions.path = reqOpts.path
+      }
+      if (reqOpts.method) {
+        reqOptions.method = reqOpts.method
+      }
+      console.log("REQ OPTS", reqOpts.headers, reqOptions);
+      aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
+      console.log("AFTER", reqOptions);
+      return reqOptions;
     },
+    // proxyReqOptDecorator(reqOpts, request) {
+    //   console.log("BEFORE", reqOpts);
+    //   const res = aws4.sign(
+    //     {
+    //       host: reqOpts.host!,
+    //       path: reqOpts.path!,
+    //       headers: {},
+    //       body: request.body instanceof Buffer ? request.body : '',
+    //     },
+    //     { accessKeyId, secretAccessKey }
+    //   );
+    //   console.log("AFTER", res);
+    //   return res;
+    // },
     proxyReqBodyDecorator(body) {
       return body instanceof Buffer ? body : ''
     }
