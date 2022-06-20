58dbbccc9239b050db96a40c002d353ec6598321
Stouffi <765497+Stouffi@users.noreply.github.com>
2022-06-20 11:07:43 +0200

    wip

--- src/index.ts
+++ src/index.ts
@@ -11,31 +11,37 @@ app.use(exp.raw({ type: "application/json" }));
 
 app.use(
   proxy(esUrl, {
+    // proxyReqOptDecorator(reqOpts, request) {
+    //   const headers = reqOpts.headers ?? {};
+    //   const { ["content-type"]: contentType, date: dateHeader } = headers;
+    //   const reqOptions: aws4.Request = {
+    //     headers: Object.assign(
+    //       contentType ? { "Content-Type": contentType } : {},
+    //       dateHeader ? { Date: dateHeader } : {}
+    //     ),
+    //   };
+    //   if (request.body instanceof Buffer) {
+    //     reqOptions.body = request.body;
+    //   }
+    //   if (reqOpts.host) {
+    //     reqOptions.host = reqOpts.host
+    //   }
+    //   if (reqOpts.path) {
+    //     reqOptions.path = reqOpts.path
+    //   }
+    //   if (reqOpts.method) {
+    //     reqOptions.method = reqOpts.method
+    //   }
+    //   console.log("REQ OPTS", reqOpts.headers, reqOptions);
+    //   aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
+    //   console.log("AFTER", reqOptions);
+    //   return reqOptions;
+    // },
     proxyReqOptDecorator(reqOpts, request) {
-      const headers = reqOpts.headers ?? {};
-      const { ["content-type"]: contentType, date: dateHeader } = headers;
-      const reqOptions: aws4.Request = {
-        headers: Object.assign(
-          contentType ? { "Content-Type": contentType } : {},
-          dateHeader ? { Date: dateHeader } : {}
-        ),
-      };
-      if (request.body instanceof Buffer) {
-        reqOptions.body = request.body;
-      }
-      if (reqOpts.host) {
-        reqOptions.host = reqOpts.host
-      }
-      if (reqOpts.path) {
-        reqOptions.path = reqOpts.path
-      }
-      if (reqOpts.method) {
-        reqOptions.method = reqOpts.method
-      }
-      console.log("REQ OPTS", reqOpts.headers, reqOptions);
-      aws4.sign(reqOptions, { accessKeyId, secretAccessKey });
-      console.log("AFTER", reqOptions);
-      return reqOptions;
+      
+      aws4.sign(reqOpts as any, { accessKeyId, secretAccessKey });
+      // console.log("AFTER", reqOptions);
+      return reqOpts;
     },
   })
 );
